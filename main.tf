cdad0cb903f40139de31da1c54ef109dc64c23a3
mountaincenter <y.hiroyuki560730@gmail.com>
2023-02-25 08:33:17 +0900

    Backend Configuration

---
+++ main.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "aws-ecs-terraform-tfstate-yam"
+    key = "terraform.tfstate"
+    region = "ap-northeast-1"
+    dynamodb_table = "aws-ecs-terraform-tfstate-locking"
+    encrypt = true
+  }
+}
